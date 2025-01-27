8544a36315dfb434dd428b0e389752b02e5a791d
Mite Gjorgjiev <m.gjorgjiev@mce-solutions.it>
2025-01-27 11:57:53 +0100

    refactor: :recycle: Spostate interface in file separati

--- src/crudify/interface/crudify.interface.ts
+++ src/crudify/interface/crudify.interface.ts
@@ -1,19 +1,7 @@
-import { Type } from "@nestjs/common";
-import { ControllerMethods } from "./controllermethods.type";
-import { IRouteConfig } from "./routeconfig.interface";
-
-import {Schema} from 'mongoose';
+import { ICrudifyModel } from "./model.interface";
+import { ICrudifyRoutes } from "./routes.interface";
 
 export interface ICrudify {
-  model: {
-    type: Type;
-    cdto?: Type;
-    udto?: Type;
-    schema?:Schema
-  };
-  routes?: {
-    config?: Partial<Record<ControllerMethods, IRouteConfig>>;
-    exclude?: ControllerMethods[];
-    decorators?: MethodDecorator[];
-  };
+  model: ICrudifyModel;
+  routes?: ICrudifyRoutes;
 }
---
+++ src/crudify/interface/routes.interface.ts
@@ -0,0 +1,8 @@
+import { ControllerMethods } from "./controllermethods.type";
+import { IRouteConfig } from "./routeconfig.interface";
+
+export interface ICrudifyRoutes {
+  config?: Partial<Record<ControllerMethods, IRouteConfig>>;
+  exclude?: ControllerMethods[];
+  decorators?: MethodDecorator[];
+}
