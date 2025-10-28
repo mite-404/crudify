57efd1c235c02c28c5755ca30850180387f451b9
Mite <m.gjorgjiev@mce-solutions.it>
2025-10-28 11:59:54 +0100

    refactor: :recycle: Esportate altri file in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 export * from "./crudify/crudify.controller";
 export * from "./crudify/crudify.service";
 export * from "./crudify/crudify.decorator";
+export * from "./crudify/disable.guard";
+export * from "./crudify/query-parser.namespace";
 export * from "./crudify/interface/crudify.interface";
+export * from "./crudify/interface/response.interface";
+export * from "./crudify/interface/routes.interface";
 
 export * from "./crudify/crudify.module";
